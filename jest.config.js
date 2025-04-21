d355c5f6d9dfb42a82ded07995ce9dce75bf26eb
Kishan Ghetiya <kishanp.ghetiya@gmail.com>
2025-04-21 17:47:54 +0530

    Add test coverage setup and configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "node"],
   testMatch: ["**/tests/**/*.test.ts"],
 };
